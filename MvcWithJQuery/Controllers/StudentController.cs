c1d5264c21a05cbc576ce52b28a6418f7250a89a
priyanshu <priyanshuarya17@gmail.com>
2023-09-19 23:21:14 +0530

    Add project files.

---
+++ MvcWithJQuery/Controllers/StudentController.cs
@@ -0,0 +1,46 @@
+﻿using MvcWithJQuery.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace MvcWithJQuery.Controllers
+{
+    public class StudentController : Controller
+    {
+        // GET: Student
+        [HttpGet]
+        public ActionResult AllStudents()
+        {
+            List<Student> students= ContextDb.GetInstance().GetStudents();
+            return View(students);  
+        }
+
+        [HttpGet]
+        public ActionResult InsertData()
+        {
+            return View();
+        }
+        [HttpPost]
+        public ActionResult InsertData(Student student)
+        {
+            ContextDb.GetInstance().InsertStudent(student);
+            return RedirectToAction("AllStudents", "Student");
+        }
+
+
+        [HttpGet]
+        public ActionResult Update(int Id)
+        {
+          Student std=  ContextDb.GetInstance().GetSingleStudent(Id);
+            return View(std);
+        }
+        [HttpPost]
+        public ActionResult Update(Student student)
+        {
+            ContextDb.GetInstance().UpdateStudent(student);
+            return RedirectToAction("AllStudents", "Student");
+        }
+    }
+}
---
+++ MvcWithJQuery/Models/ContextDb.cs
@@ -0,0 +1,125 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Configuration;
+using System.Data.SqlClient;
+using System.Linq;
+using System.Web;
+using System.Data;
+
+namespace MvcWithJQuery.Models
+{
+    public sealed class ContextDb
+    {
+        #region Singleton
+        private static ContextDb Instance = null;
+        public static ContextDb GetInstance()
+        {
+            if (Instance == null)
+            {
+                Instance = new ContextDb();
+            }
+            return Instance;
+        }
+        private ContextDb()
+        {
+
+        }
+        #endregion
+        string str = ConfigurationManager.ConnectionStrings["constr"].ConnectionString;
+
+        public List<Student> GetStudents()
+        {
+            List<Student> list = new List<Student>();
+            using (SqlConnection con = new SqlConnection())
+            {
+                con.ConnectionString = str;
+                using (SqlCommand cmd= new SqlCommand())
+                {
+                    cmd.Connection = con;
+                    cmd.CommandType = CommandType.Text;
+                    cmd.CommandText = "select * from Students";
+                    SqlDataAdapter da = new SqlDataAdapter(cmd);
+                    DataTable dt = new DataTable();
+                    da.Fill(dt);
+                    foreach(DataRow dr in dt.Rows)
+                    {
+                        list.Add(new Student
+                        {
+                            Id = (int)dr["Id"],
+                            Name = dr["Name"].ToString(),
+                            Age = (int)dr["Age"]
+                        });
+                    }
+                    return list;
+                }
+            }
+        }
+
+
+        public Student GetSingleStudent(int Id)
+        {
+            Student student= new Student();
+            using (SqlConnection con = new SqlConnection())
+            {
+                con.ConnectionString = str;
+                using (SqlCommand cmd = new SqlCommand())
+                {
+                    cmd.Connection = con;
+                    cmd.CommandType = CommandType.Text;
+                    cmd.CommandText = "select * from Students where Id='"+Id+"'";
+                    SqlDataAdapter da = new SqlDataAdapter(cmd);
+                    DataTable dt = new DataTable();
+                    da.Fill(dt);
+                    foreach (DataRow dr in dt.Rows)
+                    {
+                      student.Id= (int)dr["Id"];    
+                        student.Name= dr["Name"].ToString();
+                        student.Age= (int)dr["Age"];  
+                    }
+                    return student;
+                }
+            }
+        }
+
+        public void InsertStudent(Student student)
+        {
+            using (SqlConnection con = new SqlConnection())
+            {
+                con.ConnectionString = str;
+                using (SqlCommand cmd = new SqlCommand())
+                {
+                    cmd.CommandType = CommandType.Text;
+                    cmd.CommandText = "insert into Students values('" + student.Name + "','" + student.Age + "')";
+                    cmd.Connection = con;
+                    if (con.State == ConnectionState.Closed)
+                    {
+                        con.Open();
+                    }
+                    cmd.ExecuteNonQuery();
+                    con.Close();
+                }
+            }
+        }
+
+        public void UpdateStudent(Student student)
+        {
+            using (SqlConnection con = new SqlConnection())
+            {
+                con.ConnectionString = str;
+                using (SqlCommand cmd = new SqlCommand())
+                {
+                    cmd.CommandType = CommandType.Text;
+                    cmd.CommandText = "update Students set Name='"+student.Name+"',Age='"+student.Age+"' where Id='"+student.Id+"'";
+                    cmd.Connection = con;
+                    if (con.State == ConnectionState.Closed)
+                    {
+                        con.Open();
+                    }
+                    cmd.ExecuteNonQuery();
+                    con.Close();
+                }
+            }
+        }
+
+    }
+}
